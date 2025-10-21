1925a1d3ed09c446b8954740fd19ff5057eecd0b
Cortinovis <160122402+shortinovis@users.noreply.github.com>
2025-10-21 10:36:16 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -1,16 +1,17 @@
 <?php
-if(isset($_POST["nomeutente"])&& !empty(trim($_POST["nomeutente"])) && isset($_POST["password"])&& !empty(trim($_POST["password"]))){
-    $nomeutete=trim($_POST["nomeutente"]);
-    $password=trim($_POST["password"]);
-
-    $log_arr=json_decode(file_get_contents("salvataggio.txt"), true);
-    if(!is_array($logs_arr)){
-        $logs_arr=[];
+    session_start();
+    if(isset($_POST["username"]) && !empty(trim($_POST["username"])) && isset($_POST["password"]) && !empty(trim($_POST["password"]) && isset($_POST["password2"]) && !empty(trim($_POST["password2"])) && trim($_POST["password2"])===trim($_POST["password"]))){
+        $username = trim($_POST["username"]);
+        $password = trim($_POST["password"]);
+        //lettura da file
+        $logs_arr=letturaFile("data/user.txt");
+        if (!isset($logs_arr[$username])){ 
+            $logs_arr[$username]=password_hash($password,PASSWORD_DEFAULT); 
+            scritturaFile("data/user.txt",$logs_arr);
+            header("location: logAcc.php");
+        } else
+            echo $pagina_errore;
+    } else{
+        echo $pagina_errore;
     }
-    if(isset($logs_arr[$username]) && logs_arr[$password]===trim($password)){
-        header("location: home.php");
-    }else{
-
-    }
-}
 ?>
